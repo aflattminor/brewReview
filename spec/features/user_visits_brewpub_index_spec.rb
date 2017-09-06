f272eee3b44932ff2c020a9764d063d24b9c3e82
Alex Luna <alexluna@Alexs-MacBook-Air.local>
2017-09-06 10:22:01 -0400

    removed white space

--- spec/features/user_visits_brewpub_index_spec.rb
+++ spec/features/user_visits_brewpub_index_spec.rb
@@ -1,20 +1,16 @@
 require 'rails_helper'
 
 feature "user visits brewpub index" do
-
   scenario "user visits the index page" do
     brewpub = create(:brewpub)
     visit "/"
-
     expect(page).to_not have_content("Yards")
     expect(page).to_not have_content("901 N Delaware Ave")
     expect(page).to_not have_content("Philadelphia")
     expect(page).to_not have_content("PA")
     expect(page).to_not have_content("19123")
     expect(page).to_not have_content("A beer co.")
-
     visit "/brewpubs"
-
     expect(page).to have_current_path("/brewpubs")
     expect(page).to have_content("Yards")
     expect(page).to have_content("901 N Delaware Ave")
@@ -22,7 +18,5 @@ feature "user visits brewpub index" do
     expect(page).to have_content("PA")
     expect(page).to have_content("19123")
     expect(page).to have_content("A beer co.")
-
   end
-
 end
