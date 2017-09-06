e0180983fd67cbe6c57f653e0cc2d57d76c061a0
Alex Luna <alexluna@Alexs-MacBook-Air.local>
2017-09-06 11:07:56 -0400

    standardized db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,37 @@
-# require 'faker'
-#
-# 5.times do
-#   Brewpub.create(
-#     name:
-#     address:
-#     city:
-#     state:
-#     zip:
-#     description:
-#     website_url:
-#     img_url:
-#     logo_url:
-#     phone_number:
-#     contact_email:
-#     creator_id:
-#     rating:
-#     hours:
-#     beers:
-#     tours:
-#     facebook_url:
-#     twitter_url:
-#     instagram_url:
-#   )
-# end
+Brewpub.create(
+  name: 'Philadelphia Brewing Co.',
+  address: '2440 Frankford Ave',
+  city: 'Philadelphia',
+  state: 'PA',
+  zip: '19125',
+  description: 'A brew company in Philly.',
+  website_url: 'http://philadelphiabrewing.com/',
+  img_url: 'http://www.ballparksandbrews.com/wp-content/uploads/2011/08/100_5781.jpg',
+  logo_url: 'http://philadelphiabrewing.com/wp-content/uploads/2016/12/PhilaBrewLogo.png',
+  phone_number: '(215)427-2739'
+)
+
+Brewpub.create(
+  name: 'Yards Brewing Company',
+  address: '901 N Delaware Ave',
+  city: 'Philadelphia',
+  state: 'PA',
+  zip: '19123',
+  description: '',
+  website_url: 'http://www.yardsbrewing.com/',
+  img_url: 'http://philly.thedrinknation.com/images/stores/YardsBrewing.jpg',
+  logo_url: 'http://www.yardsbrewing.com/assets/img/yards-logo-home.svg',
+  phone_number: '(215)634-2600'
+)
+
+User.create(
+  email: "MichaelJennings@gmail.com",
+  password: "123456",
+  password_confirmation: "123456"
+)
+
+User.create(
+  email: "AlexLuna@gmail.com",
+  password: "123456",
+  password_confirmation: "123456"
+)
