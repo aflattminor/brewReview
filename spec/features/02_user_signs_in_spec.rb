4f63cd6aacc44de832f5a74826242fb161b88770
aflattminor <ajflattery50@gmail.com>
2017-09-06 10:08:51 -0400

    user authentication with testing

---
+++ spec/features/02_user_signs_in_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+feature 'user sign in', %Q{
+  As a user
+  I want to sign in
+  So I can add a Brewpub
+} do
+
+  scenario 'an existing user specifies a valid email and password' do
+    user = FactoryGirl.create(:user)
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button 'Log in'
+
+    expect(page).to have_content("Welcome Back!")
+    expect(page).to have_content("Sign out")
+  end
+
+  scenario 'a nonexistent email and password is supplies' do
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email", with: "IdontExist@gmail.com"
+    fill_in "Password", with: "password"
+    click_button 'Log in'
+
+    expect(page).to have_content("Sign in Invalid Email or password")
+    expect(page).to have_content("Log in")
+  end
+
+  scenario 'an existing email and wrong password is supplies and denies access' do
+  visit root_path
+  click_link "Sign in"
+  fill_in "Email", with: "junk@hotmail.com"
+  fill_in "Password", with: "junknond"
+  click_button 'Log in'
+
+  expect(page).to have_content("Invalid Email or password.")
+  expect(page).to have_content("Log in")
+end
+  scenario 'an already authenticated user cannot re-sign in' do
+      user = FactoryGirl.create(:user)
+      visit root_path
+      click_link "Sign in"
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      click_button 'Log in'
+      
+
+
+      expect(page).to_not have_content("Sign in")
+      expect(page).to have_content("Sign out")
+    end
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -62,3 +62,7 @@ RSpec.configure do |config|
     config.include FactoryGirl::Syntax::Methods
 end
 
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+end
