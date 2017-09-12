eabaa22dab12464d55d7c262524058c8f16f270c
Alex Luna <alexluna@Alexs-MacBook-Air.local>
2017-09-12 15:36:53 -0400

    local emails and testing

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -8,19 +8,25 @@ class Api::V1::ReviewsController < ApplicationController
 
   def create
     data = JSON.parse(request.body.read)
+
     if current_user
       new_review = Review.create(
         rating: data["rating"],
         header: data["header"],
         body: data["body"],
         brewpub_id: data["brewpub_id"],
-        user_id: current_user.id, #needs user authentication feature from devise
+        user_id: current_user.id,
         vote_count: 0
       )
-      render json: new_review
     else
       render json: nil
     end
+
+    if new_review.save
+      ReviewMailer.new_review(new_review).deliver_now
+      render json: new_review
+    end
+
   end
 
 end
--- spec/controllers/api/v1/reviews_controller_spec.rb
+++ spec/controllers/api/v1/reviews_controller_spec.rb
@@ -6,12 +6,11 @@ RSpec.describe Api::V1::ReviewsController, type: :controller do
   let!(:newReview) { Review.create(rating: 3, header: "Here's a Review Header", body: "I got your review right here buddy.", brewpub_id: 1, user_id: 1, vote_count: 0) }
 
   describe "POST#create" do
-    it "returns the json of the newly posted review" do
+    it "returns the json of the newly posted review and sends email" do
 
       sign_in user
 
       post :create, params: { brewpub_id: 1, user_id: 1}, body: {rating: 3, header: "Here's a Review Header", body: "I got your review right here buddy.", brewpub_id: 1, user_id: 1, vote_count: 0}.to_json, format: :json
-
 
       returned_json = JSON.parse(response.body)
       expect(response.status).to eq 200
@@ -21,6 +20,9 @@ RSpec.describe Api::V1::ReviewsController, type: :controller do
       expect(returned_json).to_not be_kind_of(Array)
       expect(returned_json["rating"]).to eq 3
       expect(returned_json["body"]).to eq "I got your review right here buddy."
+
+      # expect(page).to have_content("I got your review right here buddy.")
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
 
     end
   end
