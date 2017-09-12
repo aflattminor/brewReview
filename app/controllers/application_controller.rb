ad1c971acc0a362295a88190c96ae881103612b6
Alex Luna <alexluna@Alexs-MacBook-Air.local>
2017-09-12 11:10:10 -0400

    local profile photo upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
   before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :null_session
   before_action :initialize_session
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_photo, :profile_photo_cache, :remove_profile_photo])
   end
 
   def authorize_user
